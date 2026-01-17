8d145192922f3f3b5a12339a816fded498b65b5e
cedlark <cedricbretstephens@gmail.com>
2026-01-17 10:46:48 -0700

    working in the ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -9,7 +9,10 @@ package chess;
 public class ChessPosition {
     private int row;
     private int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -17,7 +20,9 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+
+        return row;
+
     }
 
     /**
@@ -25,6 +30,8 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+
+        return col;
+
     }
 }
