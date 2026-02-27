e62bde017218afaaf49b1f6e7bc8e4ba4361c075
cedlark <cedricbretstephens@gmail.com>
2026-02-27 13:17:12 -0700

    adding in service classes

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,14 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+
 public class ClearService {
+    private final MemoryDataAccess dao;
+    public ClearService(MemoryDataAccess dao) {
+        this.dao = dao;
+    }
+    public void clear() throws DataAccessException {
+        dao.clear();
+    }
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,15 +1,31 @@
 package service;
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import model.*;
 
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {
-        try{
-            var port = 8080;
+    private final MemoryDataAccess dao;
 
-            MemoryDataAccess dataAccess = new MemoryDataAccess();
-            user = dataAccess.getUser()
+    public UserService(MemoryDataAccess dao){
+        this.dao = dao;
+    }
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        String username = registerRequest.username();
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+        if (username == null || password == null || email == null ||
+                username.isBlank() || password.isBlank() || email.isBlank()){
+            throw new DataAccessException("Error: Bad request");
         }
+        if (dao.getUser(username) != null){
+            throw new DataAccessException("Error: Already Taken");
+        }
+        UserData newUser = new UserData(username, password, email);
+        dao.addUser(newUser);
+        String token = MemoryDataAccess.generateToken();
+        AuthData auth = new AuthData(token, username);
+        dao.addAuth(auth);
+        return new RegisterResult(username, token);
 
     }
     public LoginResult login(LoginRequest loginRequest) {
