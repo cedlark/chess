de6537ecb1ad9682bd4fccd6b48a252cd68fdc2d
cedlark <cedricbretstephens@gmail.com>
2026-03-11 14:23:46 -0600

    fixed the problems

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -15,30 +15,35 @@ public class Server {
 
     private final Javalin javalin;
 
-    public Server() throws DataAccessException {
+    public Server() {
         javalin = Javalin.create(config -> {
             config.staticFiles.add("web");
             config.jsonMapper(new JavalinGson(new Gson(), false));
         });
-        DataAccess dao = new MySqlDataAccess();
-        UserService userService = new UserService(dao);
-        GameService gameService = new GameService(dao);
-        ClearService clearService = new ClearService(dao);
-        // Register your endpoints and exception handlers here.
-        RegisterHandler registerHandler = new RegisterHandler(userService);
-        LoginHandler loginHandler = new LoginHandler(userService);
-        LogoutHandler logoutHandler = new LogoutHandler(userService);
-        ListGamesHandler listGamesHandler = new ListGamesHandler(gameService);
-        CreateGameHandler createGameHandler = new CreateGameHandler(gameService);
-        JoinGameHandler joinGameHandler = new JoinGameHandler(gameService);
-        ClearHandler clearHandler = new ClearHandler(clearService);
-        javalin.post("/user", registerHandler::handle);
-        javalin.post("/session", loginHandler::handle);
-        javalin.delete("/session", logoutHandler::handle);
-        javalin.get("/game", listGamesHandler::handle);
-        javalin.post("/game", createGameHandler::handle);
-        javalin.put("/game", joinGameHandler::handle);
-        javalin.delete("/db", clearHandler::handle);
+        try {
+
+            DataAccess dao = new MySqlDataAccess();
+            UserService userService = new UserService(dao);
+            GameService gameService = new GameService(dao);
+            ClearService clearService = new ClearService(dao);
+            // Register your endpoints and exception handlers here.
+            RegisterHandler registerHandler = new RegisterHandler(userService);
+            LoginHandler loginHandler = new LoginHandler(userService);
+            LogoutHandler logoutHandler = new LogoutHandler(userService);
+            ListGamesHandler listGamesHandler = new ListGamesHandler(gameService);
+            CreateGameHandler createGameHandler = new CreateGameHandler(gameService);
+            JoinGameHandler joinGameHandler = new JoinGameHandler(gameService);
+            ClearHandler clearHandler = new ClearHandler(clearService);
+            javalin.post("/user", registerHandler::handle);
+            javalin.post("/session", loginHandler::handle);
+            javalin.delete("/session", logoutHandler::handle);
+            javalin.get("/game", listGamesHandler::handle);
+            javalin.post("/game", createGameHandler::handle);
+            javalin.put("/game", joinGameHandler::handle);
+            javalin.delete("/db", clearHandler::handle);
+        } catch (DataAccessException e) {
+            System.out.println("Failed to start server: " + e.getMessage());
+        }
     }
 
     public int run(int desiredPort) {
