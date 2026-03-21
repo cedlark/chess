336bd80df8dcbd334a3d523eaca5c248652c3fb4
cedlark <cedricbretstephens@gmail.com>
2026-03-21 12:12:06 -0600

    added in build request from petShop

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -3,8 +3,11 @@ package client;
 import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
+import service.RegisterRequest;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +20,10 @@ public class ServerFacade {
     }
 
     public AuthData register(String username, String password, String email){
-        var body = Map.of("username",username,"password",password,"email",email);
-        String json = gson.toJson(body);
+        var body = new RegisterRequest(username, password, email);
+        var request = buildRequest("POST","/user",body,null);
+        var response = sendRequest(request);
+        return handleResponse(response);
     }
 
     public AuthData login(String username, String password)
@@ -29,5 +34,24 @@ public class ServerFacade {
 
     public void createGame(String authToken, String gameName)
 
-    public void joinGame(String authToken, String color, int gameID)
+    public void joinGame(String authToken, String color, int gameID){
+
+    }
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
 }
