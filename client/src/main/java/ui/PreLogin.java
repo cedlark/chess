17cc11210f3988323c15c1a0820b002f7e886700
cedlark <cedricbretstephens@gmail.com>
2026-03-21 11:43:02 -0600

    Adding in login and register

--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -7,9 +7,9 @@ import model.AuthData;
 import java.util.Scanner;
 
 public class PreLogin {
-    private ServerFacade server;
-    private Scanner scanner;
-    private ClientMain client;
+    private final ServerFacade server;
+    private final Scanner scanner;
+    private final ClientMain client;
 
     public PreLogin(ServerFacade server, Scanner scanner, ClientMain client){
         this.server = server;
@@ -32,12 +32,34 @@ public class PreLogin {
         }
     }
     public void help(){
-
+        System.out.println("Commands:");
+        System.out.println("help - show commands");
+        System.out.println("login - login user");
+        System.out.println("register - create account");
+        System.out.println("quit - exit");
     }
     public void login(){
+        try{
+            System.out.print("Enter Username: ");
+            String username = scanner.nextLine();
+            System.out.print("Enter Password: ");
+            String password = scanner.nextLine();
+            var authData = server.login(username,password);
+            client.loginSuccess(authData.getAuthToken());
+
+        } catch(Exception e){
+            System.out.println("Login Failed:");
+        }
 
     }
     public void register(){
-
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+        var authData = server.register(username, password, email);
+        client.loginSuccess(authData.getAuthToken());
     }
 }
