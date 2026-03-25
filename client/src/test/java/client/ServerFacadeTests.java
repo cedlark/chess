6e07f65e94c245ef7f1490bf818716a6e4f71795
cedlark <cedricbretstephens@gmail.com>
2026-03-25 12:49:11 -0600

    Register tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,50 @@
 package client;
 
+import model.AuthData;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
     static void stopServer() {
         server.stop();
     }
+    @BeforeEach
+    void clearDatabase() throws Exception {
+
+    }
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerSuccess() throws Exception {
+        AuthData auth = facade.register("player1", "password", "p1@email.com");
+        assertNotNull(auth);
+        assertNotNull(auth.getAuthToken());
+        assertEquals("player1", auth.getUsername());
     }
+
+    @Test
+    void registerFailure() throws Exception {
+        facade.register("player1", "password", "p1@email.com");
+        Exception ex = assertThrows(Exception.class, () ->
+                facade.register("player1", "password", "p1@email.com"));
+        assertNotNull(ex.getMessage());
+    }
+
 
 }
